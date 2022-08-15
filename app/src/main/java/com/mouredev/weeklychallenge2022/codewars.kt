5316f464b4b8604a4371a29dbdf8b5ffec3eb2d6
David Salazar <saladavid0110@gmail.com>
2022-08-15 16:38:21 -0300

    CodeWars

---
+++ app/src/main/java/com/mouredev/weeklychallenge2022/codewars.kt
@@ -0,0 +1,15 @@
+package com.mouredev.weeklychallenge2022
+
+
+fun main() {
+    println(
+        inArray(
+            arrayOf("arp", "live", "strong"),
+            arrayOf("lively", "alive", "harp", "sharp", "armstrong")
+        )
+    )
+}
+
+fun inArray(array1: Array<String>, array2: Array<String>): Array<String> {
+    return array1
+}
